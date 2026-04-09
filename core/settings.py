b66732a83555bb439db1efaf948a8749fa0a53fd
sadiki360 <maroua.sadiki@etu.uae.ac.ma>
2026-04-09 19:11:27 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -83,3 +83,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-4565e.up.railway.app',
+]
